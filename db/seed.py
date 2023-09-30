7ffe6d45f1f0aca3751631b28bab4be104243d59
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-09-30 09:04:06 +0300

    modified my seed.py file to be in python

--- db/seed.py
+++ db/seed.py
@@ -1,35 +1,38 @@
-puts "🦸‍♀️ Seeding powers..."
-Power.create([
-  { name: "super strength", description: "gives the wielder super-human strengths" },
-  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
-  { name: "elasticity", description: "can stretch the human body to extreme lengths" }
-])
+import random
+from app.models import Power, Hero, HeroPower
 
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
+print("🦸‍♀️ Seeding powers...")
+powers_data = [
+    {"name": "super strength", "description": "gives the wielder super-human strengths"},
+    {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+    {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+    {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+]
 
-puts "🦸‍♀️ Adding powers to heroes..."
+Power.objects.bulk_create([Power(**data) for data in powers_data])
 
+print("🦸‍♀️ Seeding heroes...")
+heroes_data = [
+    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"}
+]
+
+Hero.objects.bulk_create([Hero(**data) for data in heroes_data])
+
+print("🦸‍♀️ Adding powers to heroes...")
 strengths = ["Strong", "Weak", "Average"]
-Hero.all.each do |hero|
-  rand(1..3).times do
-    # get a random power
-    power = Power.find(Power.pluck(:id).sample)
 
-    HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
-  end
-end
+for hero in Hero.objects.all():
+    for _ in range(random.randint(1, 3)):
+        power = Power.objects.order_by('?').first()
+        HeroPower.objects.create(hero=hero, power=power, strength=random.choice(strengths))
 
-puts "🦸‍♀️ Done seeding!"
+print("🦸‍♀️ Done seeding!")
