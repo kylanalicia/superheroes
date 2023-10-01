08c38ac4b41434675a1ba0a4662698da9ebdefa1
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2023-10-01 11:18:08 +0300

    made a hero model in the models file

--- app/models.py
+++ app/models.py
@@ -6,5 +6,8 @@ class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255),nullable=False)
+    super_name = db.Column(db.String(255),nullable=False)
+    powers = db.relationship('HeroPower',back_popukates='hero',overlaps='hero')
 
 # add any models you may need. 
